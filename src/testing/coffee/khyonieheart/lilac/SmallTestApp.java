7cc5c7a44a23b7d1d471e60c9d481c7970790d22
Khyonie <khyonie@proton.me>
2026-01-11 21:48:40 -0700

    Fix TomlConfiguration key parser rejecting valid keys

--- src/testing/coffee/khyonieheart/lilac/SmallTestApp.java
+++ src/testing/coffee/khyonieheart/lilac/SmallTestApp.java
@@ -1,36 +1,39 @@
 package coffee.khyonieheart.lilac;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.util.Map;
-
 public class SmallTestApp
 {
+	private static final String[] TEST_KEYS = {
+		"key",
+		"bare_key",
+		"bare-key",
+		"_underscore",
+		"a1b2c3",
+		"true",
+		"false",
+		"has spaces",
+		"\"has.dots.inside\"",
+		"\"127.0.0.1\"",
+		"\"quote \" inside\"",
+		"\"tab\tand\nnewline\"",
+		"\"\u03C0\"",
+		"'no escapes here \n \t'",
+		"'contains \"double quotes\"'",
+		"'path C:\\Users\\Hailey'",
+		"a.b.c",
+		"site.\"google.com\".status",
+		"servers.'eu-west-1'.ip",
+		"\"root-key\".child.\"leaf-key\""
+	};
 	public static void main(String[] args) throws Exception
 	{
-		String file = Files.readString(new File("./project.toml").toPath());
-
-		TomlDecoder decoder = new LilacDecoder(TomlVersion.V1_1_0);
-		Map<String, Object> data = decoder.decode(new Document(file));
-		printMap(data, 0);
-	}
-
-	@SuppressWarnings("unchecked")
-	private static void printMap(
-		Map<String, Object> data,
-		int depth
-	) {
-		for (String key : data.keySet())
+		for (String testKey : TEST_KEYS)
 		{
-			Object value = data.get(key);
-			if (value instanceof Map)
+			System.out.println("Key: " + testKey);
+			String[] output = TomlUtilities.fullyQualifiedKeyToArray(testKey);
+			for (String subkey : output)
 			{
-				System.out.println("- ".repeat(depth) + key + ":");
-				printMap((Map<String, Object>) value, depth + 1);
-				continue;
+				System.out.println("- " + subkey);
 			}
-
-			System.out.println("- ".repeat(depth) + key + " = " + value);
 		}
 	}
 }
